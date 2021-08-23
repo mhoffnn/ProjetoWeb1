f1dbcfd0d953afaa7cd3086c19d1298abbebdfe8
mhoffnn <mhoffnn@gmail.com>
2021-08-22 23:30:12 -0300

    Visitante
    Criação das telas de cadastrar/editar/exclui visitante

--- src/App.js
+++ src/App.js
@@ -3,19 +3,26 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/home';
 
-import CadastrarAluno from './pages/cadastro/aluno/cad_aluno.js';
-import EditarAluno from './pages/editar/aluno/edit_aluno.js';
-import CadastrarEstacionamento from './pages/cadastro/estacionamento/cad_estacionamento.js';
-import EditarEstacionamento from './pages/editar/estacionamento/edit_estacionamento.js';
+import CadastrarAluno from './pages/cadastro/aluno/cad_aluno';
+import CadastrarVisitante from './pages/cadastro/vizitante/cad_visitante';
+import CadastrarEstacionamento from './pages/cadastro/estacionamento/cad_estacionamento';
+
+import EditarAluno from './pages/editar/aluno/edit_aluno';
+import EditarVisitante from './pages/editar/vizitante/edit_visitante';
+import EditarEstacionamento from './pages/editar/estacionamento/edit_estacionamento';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/editar_aluno/:id_aluno" component={EditarAluno} />
-        <Route path="/cadastrar_aluno" component={CadastrarAluno} />
+        <Route path="/editar_visitante/:id_visitante" component={EditarVisitante} />
         <Route path="/editar_estacionamento/:id_estacionamento" component={EditarEstacionamento} />
-        <Route path="/cadastrar_estacionamento" component={CadastrarrEstacionamento} />
+
+        <Route path="/cadastrar_aluno" component={CadastrarAluno} />
+        <Route path="/cadastrar_visitante" component={CadastrarVisitante} />
+        <Route path="/cadastrar_estacionamento" component={CadastrarEstacionamento} />
+
         <Route path="/" component={Home} />
       </Switch>
     </Router>
