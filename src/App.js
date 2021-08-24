05c49062bde019084974d6ec6d7c221bd515f104
mhoffnn <mhoffnn@gmail.com>
2021-08-23 22:51:42 -0300

    update

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { DataProvider } from './contexts/data';
 
-import CadastrarServidor from './pages/cadastro/servidor/cad_servidor';
-import EditarServidor from './pages/editar/servidor/edit_servidor';
 import CadastrarAluno from './pages/cadastro/aluno/cad_aluno.js';
-import EditarAluno from './pages/editar/aluno/edit_aluno.js';
-import CadastrarEstacionamento from './pages/cadastro/estacionamento/cad_estacionamento.js';
-import EditarEstacionamento from './pages/editar/estacionamento/edit_estacionamento.js';
+import CadastrarServidor from './pages/cadastro/servidor/cad_servidor';
 import CadastrarVisitante from './pages/cadastro/vizitante/cad_visitante';
+import CadastrarEstacionamento from './pages/cadastro/estacionamento/cad_estacionamento.js';
+
+import EditarAluno from './pages/editar/aluno/edit_aluno.js';
+import EditarServidor from './pages/editar/servidor/edit_servidor';
 import EditarVisitante from './pages/editar/vizitante/edit_visitante';
+import EditarEstacionamento from './pages/editar/estacionamento/edit_estacionamento.js';
 
 import Home from './pages/home';
 
@@ -20,14 +21,15 @@ function App() {
     <DataProvider>
       <Router>
         <Switch>
-          <Route path="/cadastrar_servidor" component={CadastrarServidor} />
-          <Route path="/editar_servidor/:id_servidor" component={EditarServidor} />
           <Route path="/cadastrar_aluno" component={CadastrarAluno} />
-          <Route path="/editar_aluno/:id_aluno" component={EditarAluno} />
-          <Route path="/cadastrar_estacionamento" component={CadastrarEstacionamento} />
-          <Route path="/editar_estacionamento/:id_estacionamento" component={EditarEstacionamento} />
+          <Route path="/cadastrar_servidor" component={CadastrarServidor} />
           <Route path="/cadastrar_visitante" component={CadastrarVisitante} />
+          <Route path="/cadastrar_estacionamento" component={CadastrarEstacionamento} />
+
+          <Route path="/editar_aluno/:id_aluno" component={EditarAluno} />
+          <Route path="/editar_servidor/:id_servidor" component={EditarServidor} />
           <Route path="/editar_visitante/:id_visitante" component={EditarVisitante} />
+          <Route path="/editar_estacionamento/:id_estacionamento" component={EditarEstacionamento} />
 
           <Route path="/" component={Home} />
         </Switch>
