098be04596c4167e6e9016bcdec44411e31a4015
mhoffnn <mhoffnn@gmail.com>
2021-08-26 00:41:19 -0300

    Cadastros responsiveis
    Telas de cadastro responsiveis

--- src/pages/cadastro/senha/cad_senha.js
+++ src/pages/cadastro/senha/cad_senha.js
@@ -6,24 +6,41 @@ import logo from '../../../utf-logo.png';
 function CadastrarSenha(request, response) {
     return (
         <div>
-            <img src={logo} className="logo-direita" alt="logo" />
-            <div id='cadastro'>
+            <div class='row'>
+                <img src={logo}
+                    class='col-sm-3'
+                    id="logo-direita"
+                    alt="logo"
+                />
+
+                <nav id='navbar' class='col-sm-3 navbar-nav navbar-expand-lg navbar-light'>
+                    <span class='navbar-brand'>Cadastrar: </span>
+                    <a class='navbar-text' href='/cadastrar/aluno'>Aluno</a>
+                    <a class='navbar-text' href='/cadastrar/veiculo'>Veiculo</a>
+                    <a class='navbar-text' href='/cadastrar/servidor'>Servidor</a>
+                    <a class='navbar-text' href='/cadastrar/visitante'>Visitante</a>
+                    <a class='navbar-text' href='/cadastrar/estacionamento'>Estacionamento</a>
+                </nav>
+            </div>
+
+            <div class="container" id='cadastro'>
                 <h1 id='titulo'>
                     Cadastrar Nova Senha
                 </h1>
                 <div>
                     <input
-                        class='large-input'
-                        type='text'
+                        class='col-11'
+                        type='password'
                         placeholder='Nova Senha'
                     />
                     <input
-                        class='large-input'
-                        type='email'
+                        class='col-11'
+                        type='password'
                         placeholder='Repita a senha'
                     />
                     <button
-                        id='confirmar-cadastro'
+                        id='central'
+                        class='col-md-3'
                         value='CONFIRMAR'
                         type='submit'
                     >
--- src/pages/cadastro/veiculo/cad_veiculo.js
+++ src/pages/cadastro/veiculo/cad_veiculo.js
@@ -8,38 +8,58 @@ import { Link } from 'react-router-dom'
 function CadastrarVeiculo(request, response) {
     return (
         <div>
-            <img src={logo} className="logo-direita" alt="logo" />
-            <div id='cadastro'>
+            <div class='row'>
+                <img src={logo}
+                    class='col-sm-3'
+                    id="logo-direita"
+                    alt="logo"
+                />
+
+                <nav id='navbar' class='col-sm-3 navbar-nav navbar-expand-lg navbar-light'>
+                    <span class='navbar-brand'>Cadastrar: </span>
+                    <a class='navbar-text' href='/cadastrar/aluno'>Aluno</a>
+                    <a class='navbar-text' href='/cadastrar/veiculo'>Veiculo</a>
+                    <a class='navbar-text' href='/cadastrar/servidor'>Servidor</a>
+                    <a class='navbar-text' href='/cadastrar/visitante'>Visitante</a>
+                    <a class='navbar-text' href='/cadastrar/estacionamento'>Estacionamento</a>
+                </nav>
+            </div>
+
+            <div class="container" id='cadastro'>
                 <h1 id='titulo'>
                     Cadastrar Veiculo
                 </h1>
-                <div>
+                <div class='row'>
                     <input
+                        class='col-md-5'
                         type='text'
                         placeholder='Tipo'
                     />
                     <input
+                        class='col-md-5'
                         type='text'
                         placeholder='Modelo'
                     />
                     <input
+                        class='col-md-5'
                         type='text'
                         placeholder='Fabricante'
                     />
                     <input
+                        class='col-md-5'
                         type='text'
                         placeholder='Cor'
                     />
                     <input
-                        id='large-input'
+                        class='col-md-11'
                         type='text'
                         placeholder='Placa'
                     />
-                    <l>
-                        <Link to="/">Já cadastrado</Link>
+                    <l class='col-md-3'>
+                        <Link id='link' to="/">Já cadastrado</Link>
                     </l>
                     <button
-                        id='confirmar-cadastro'
+                        class='col-md-4'
                         value='CONFIRMAR'
                         type='submit'
                     >
