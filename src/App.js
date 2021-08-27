f4b90e828ee8877c60e4ab1aec094c9bea1ffafe
mhoffnn <mhoffnn@gmail.com>
2021-08-27 00:11:32 -0300

    home adm e Lista de itens cadastrados

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import EditarServidor from './pages/editar/servidor/edit_servidor';
 import EditarVisitante from './pages/editar/vizitante/edit_visitante';
 import EditarEstacionamento from './pages/editar/estacionamento/edit_estacionamento.js';
 
+import ItensCadastrados from './pages/listas/itens_cadastrados/itens_cadastrados';
 import AssociarVeiculo from './pages/associar_veiculo/associ_veiculo';
 import BuscarUsuario from './pages/buscar_usuario/buscar_usuario';
 import Login from './pages/login/login.js';
@@ -28,26 +29,27 @@ function App() {
     <DataProvider>
       <Router>
         <Switch>
-          <Route path="/cadastrar/aluno" component={CadastrarAluno}/>
-          <Route path="/cadastrar/veiculo" component={CadastrarVeiculo}/>
-          <Route path="/cadastrar/servidor" component={CadastrarServidor}/>
-          <Route path="/cadastrar/visitante" component={CadastrarVisitante}/>
-          <Route path="/cadastrar/estacionamento" component={CadastrarEstacionamento}/>
+          <Route path="/cadastrar/aluno" component={CadastrarAluno} />
+          <Route path="/cadastrar/veiculo" component={CadastrarVeiculo} />
+          <Route path="/cadastrar/servidor" component={CadastrarServidor} />
+          <Route path="/cadastrar/visitante" component={CadastrarVisitante} />
+          <Route path="/cadastrar/estacionamento" component={CadastrarEstacionamento} />
 
-          <Route path="/editar/aluno/:id_aluno" component={EditarAluno}/>
-          <Route path="/editar/veiculo/:id_veiculo" component={EditarVeiculo}/>
-          <Route path="/editar/servidor/:id_servidor" component={EditarServidor}/>
-          <Route path="/editar/visitante/:id_visitante" component={EditarVisitante}/>
-          <Route path="/editar/estacionamento/:id_estacionamento" component={EditarEstacionamento}/>
+          <Route path="/editar/aluno/:id_aluno" component={EditarAluno} />
+          <Route path="/editar/veiculo/:id_veiculo" component={EditarVeiculo} />
+          <Route path="/editar/servidor/:id_servidor" component={EditarServidor} />
+          <Route path="/editar/visitante/:id_visitante" component={EditarVisitante} />
+          <Route path="/editar/estacionamento/:id_estacionamento" component={EditarEstacionamento} />
 
-          <Route path="/cadastrar/senha/:id_session" component={CadastrarSenha}/>
-          <Route path="/editar/senha/:id_session" component={EditarSenha}/>
+          <Route path="/cadastrar/senha/:id_session" component={CadastrarSenha} />
+          <Route path="/editar/senha/:id_session" component={EditarSenha} />
 
-          <Route path="/buscarusuario" component={BuscarUsuario}/>
-          <Route path="/associarveiculo/:id_session" component={AssociarVeiculo}/>
-          <Route path="/login" component={Login}/>
-          <Route path="/ADM/:id_session" component={HomeAdm}/>
-          <Route path="/:id_session" component={Home}/>
+          <Route path="/lista/itenscadastrados" component={ItensCadastrados} />
+          <Route path="/buscarusuario" component={BuscarUsuario} />
+          <Route path="/associarveiculo/:id_session" component={AssociarVeiculo} />
+          <Route path="/login" component={Login} />
+          <Route path="/ADM/:id_session" component={HomeAdm} />
+          <Route path="/:id_session" component={Home} />
         </Switch>
       </Router>
     </DataProvider>
---
+++ src/pages/listas/itens_cadastrados/itens_cadastrados.js
@@ -0,0 +1,90 @@
+import '../../../styles/global-styles.css';
+
+import logo from '../../../utf-logo.png';
+
+import { useContext } from 'react';
+import { DataContext } from '../../../contexts/data';
+
+function ItensCadastrados(request, response) {
+    const [data] = useContext(DataContext);
+
+    return (
+        <div>
+            <div class='row'>
+                <img src={logo}
+                    class='col-sm-3'
+                    id="logo-direita"
+                    alt="logo"
+                />
+
+                <nav id='navbar' class='col-sm-3 navbar-nav navbar-expand-lg navbar-light'>
+                    <span class='navbar-brand'>Cadastrar: </span>
+                    <a class='navbar-text' href='/cadastrar/aluno'>Aluno</a>
+                    <a class='navbar-text' href='/cadastrar/veiculo'>Veiculo</a>
+                    <a class='navbar-text' href='/cadastrar/servidor'>Servidor</a>
+                    <a class='navbar-text' href='/cadastrar/visitante'>Visitante</a>
+                    <a class='navbar-text' href='/cadastrar/estacionamento'>Estacionamento</a>
+                </nav>
+            </div>
+
+            <div class='container' id='cadastro'>
+                <h2>Estacionamentos cadastrados:</h2>
+                <table class='table'>
+                    <thead>
+                        <tr>
+                            <th scope='col'>Cod.</th>
+                            <th scope='col'>Descrição</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            data.parkingLots.map(pl => {
+                                return (
+                                    <tr>
+                                        <th scope='row'>{pl.id}</th>
+                                        <td>{pl.description}</td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+
+
+            <div class='container' id='cadastro'>
+                <h2>Veículos cadastrados:</h2>
+                <table class='table'>
+                    <thead>
+                        <tr>
+                            <th scope='col'>Cod.</th>
+                            <th scope='col'>Tipo</th>
+                            <th scope='col'>Modelo</th>
+                            <th scope='col'>Fabricante</th>
+                            <th scope='col'>Cor</th>
+                            <th scope='col'>Placa</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            data.cars.map(c => {
+                                return (
+                                    <tr>
+                                        <th scope='row'>{c.id}</th>
+                                        <td>{c.type}</td>
+                                        <td>{c.model}</td>
+                                        <td>{c.factory}</td>
+                                        <td>{c.color}</td>
+                                        <td>{c.plate}</td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+}
+
+export default ItensCadastrados;
