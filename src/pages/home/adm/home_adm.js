3434f6e4a350f61469ef06c542ccfb0c32bb0c29
mhoffnn <mhoffnn@gmail.com>
2021-08-27 00:11:59 -0300

    home adm

--- src/pages/home/adm/home_adm.js
+++ src/pages/home/adm/home_adm.js
@@ -1,6 +1,7 @@
 import '../../../styles/global-styles.css';
 
 import logo from '../../../utf-logo.png';
+import estacionamento from '../../../estacionamento-utfpr.png';
 
 import { Link } from 'react-router-dom'
 
@@ -22,8 +23,16 @@ function HomeAdm(request, response) {
                     <a class='navbar-text' href='/cadastrar/visitante'>Visitante</a>
                     <a class='navbar-text' href='/cadastrar/estacionamento'>Estacionamento</a>
                 </nav>
+            </div>
 
-
+            <div class='row' id='image-map'>
+                <h1 id='titulo'>Registrar Ocorrência</h1>
+                <img src={estacionamento} id="map-image" width='906px' max-width='100%' height='auto' alt="a" usemap="#map" />
+                <map name="map">
+                    <area shape="rect" coords="340, 404, 883, 709" href='/ADM/c3' />
+                    <area shape="rect" coords="339, 98, 883, 403" href='/ADM/b2' />
+                    <area shape="rect" coords="31, 100, 337, 706" href='/ADM/a1' />
+                </map>
             </div>
         </div>
     );
